8c79c7e4311e8032f65d7915894f91a8a2d4e982
Kisun Lee <klee669@gatech.edu>
2026-03-12 16:12:29 -0400

    Add compute_preconditioner function and update krawczyk_test

--- src/internals/krawczyk.jl
+++ src/internals/krawczyk.jl
@@ -1,4 +1,4 @@
-export krawczyk_operator, krawczyk_test
+export krawczyk_operator, krawczyk_test, compute_preconditioner
 
 
 ####### functions for Krawczyk test
@@ -40,4 +40,43 @@ function krawczyk_test(
     K = krawczyk_operator(system,point,r, A);
     max_norm(K) < ρ
 end
+
+
+function compute_preconditioner(sys::HCSystem, x::AbstractVector{AcbFieldElem}, t)
+    x_mid = get_mid_vec(x)
+    t_mid = t isa AcbFieldElem ? get_mid(t) : CC(t)
+    J_val = evaluate_Jac(sys, x_mid, t_mid) 
+    return inv_acb(J_val)
+end
+
+function krawczyk_test(sys::HCSystem, x::AbstractVector{AcbFieldElem}, t, r::Number; rho=0.7)
+    n = length(x)
+    x_interval = x 
+    
+    A = compute_preconditioner(sys, x_interval, t)
+    
+    one_int = RR("0 +/- 1") 
+    b_int = CC(one_int, one_int)
+    B = [b_int for _ in 1:n]
+    
+    fx = evaluate_H(sys, x_interval, t)
+    
+    x_expanded = x_interval .+ (B .* CC(r))
+    Jx = evaluate_Jac(sys, x_expanded, t)
+    
+    term1 = -(A * fx) ./ CC(r)
+    
+    I_mat = Matrix{AcbFieldElem}(undef, n, n)
+    for i in 1:n, j in 1:n
+        I_mat[i,j] = (i == j ? CC(1) : CC(0))
+    end
+    
+    term2 = (I_mat - A * Jx) * B
+    
+    K = term1 + term2
+    k_norm = norm_inf(K)
+    
+    return k_norm < rho, k_norm
+end
+
 
